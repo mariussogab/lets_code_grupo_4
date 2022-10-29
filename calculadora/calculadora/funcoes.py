0a6b71c4c8c7c455b829ea4363d2026d1c591c3f
Gabriel Mariusso <mariussogab@gmail.com>
2022-10-28 21:59:18 -0300

    "adicionando funcoes"

---
+++ calculadora/calculadora/funcoes.py
@@ -0,0 +1,14 @@
+def soma(a,b):
+    return a+b
+
+def subtracao(a,b):
+    return a-b
+
+def multiplicacao(a,b):
+    return a*b
+
+def divisao(a,b):
+    try:
+        return a/b
+    except:
+        return f"nao da pra dividir por 0"
