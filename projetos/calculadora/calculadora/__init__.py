0f51a8d4578b5dd67abf53c01384fba2a3e8351d
Gabriel Mariusso <mariussogab@gmail.com>
2022-11-01 18:29:44 -0300

    'Adicionando arquivo __init__'

---
+++ projetos/calculadora/calculadora/__init__.py
@@ -0,0 +1,23 @@
+from funcoes import soma, subtracao, multiplicacao, divisao
+def calcule():
+    a = float(input('Digite o primeiro número: '))
+    b = float(input('Digite o segundo número: '))
+    op = input('Digite a operação: ')
+    switcher={
+        'soma': soma,
+        'subtracao': subtracao,
+        'multiplicacao': multiplicacao,
+        'divisao': divisao,
+        '+': soma,
+        '-': subtracao,
+        '*': multiplicacao,
+        '/': divisao
+        }
+    if op in switcher.keys():
+        resultado = switcher[op](a,b)
+        return resultado
+    else:
+        print(f'operação não reconhecida, escolha entre {list(switcher.keys())}')
+    
+
+
